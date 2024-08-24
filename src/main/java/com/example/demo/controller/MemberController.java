989fbf97dd1e9c4a2cc3cee3098e23cdd48417fd
정재익 <wodlr1207@naver.com>
2024-08-24 19:13:44 +0900

    다듬기

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -11,22 +11,46 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/main")
 public class MemberController {
 
     @Autowired
     private MemberService memberService;
+    private Model model;
 
-    @PostMapping
-    public Member createMember(@RequestParam("nick") String nick, Member member) {
+//    @PostMapping("/main")
+//    public Member createMember(@RequestParam("nick") String nick, Member member) {
+//        member.setNick(nick);
+//        return memberService.saveMember(member);
+//    }
+
+    @RequestMapping("/send")
+    public String send() {
+        return "send";
+    }
+
+    @RequestMapping("/ok")
+    public String ok() {
+        return "ok";
+    }
+
+    @PostMapping("/ok")
+    public Member createMember2(@RequestParam("nick") String nick, @RequestParam("book") String book, @RequestParam("message") String message, Member member) {
+        member.setBook(book);
+        member.setMessage(message);
         member.setNick(nick);
         return memberService.saveMember(member);
     }
 
-    @GetMapping
+    @GetMapping("/check")
     public String getUsers(Model model) {
         List<Member> members = memberService.findAllUsers();
         model.addAttribute("members", members);
+        return "check";
+    }
+
+    @RequestMapping("/main")
+    public String submitText(@RequestParam("nick") String nick, Model model) {
+        model.addAttribute("nick", nick);
         return "main";
     }
 }
